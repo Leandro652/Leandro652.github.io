54009bc1a290a626fdbb12f7c635ea32e2ac8739
Leandro652 <109769735+Leandro652@users.noreply.github.com>
2024-01-18 21:15:33 -0600

    login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+document.addEventsListener('DOMContentLoaded',function(){
+  console.log("Hello, world");
+  var inputEmail = document.getElementById('email');
+  var inputPassword = document.getElementyById('password');
+  console.log(inputEmail);
+  console.log(InputPassword);
+});
